7dce0ae26e0e0f2882c94f27c102d11b4e2fd1e2
journo10 <80216907+journo10@users.noreply.github.com>
2021-10-19 00:18:23 +0300

    18 Ekim Js Kursu Dersi

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io");
+
+let dolarDun = 9.2;
+let dolarBugun = 9.3;
+
+{
+  let dolarDun = 9.1;
+}
+console.log(dolarDun);
+
+//const =>sabit demektir.
+const euroDun = 11.2;
+
+console.log(euroDun);
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi","Kamu Konut Kredisi",];
+konutKredileri.push("Özel Konut Kredileri");
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log(konutKredileri[i]);
+}
+
+
+
+
